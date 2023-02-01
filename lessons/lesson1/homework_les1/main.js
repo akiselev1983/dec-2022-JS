35a67a091e04ccfa21b07748a9c74e121ec77ab3
akiselev1983 <akiselev.brd@gmail.com>
2023-02-01 16:49:12 +0200

    add dec-2022-js/lesson1

---
+++ lessons/lesson1/homework_les1/main.js
@@ -0,0 +1,41 @@
+let str = "hello";
+let str1 = 'owu';
+let str2 = 'com';
+let str3 = 'ua';
+let num = 1;
+let num1 = 10;
+let num2 = -999;
+let num3 = 123;
+const num4 = 3.14;
+let num5 = 2.7;
+let num6 = 16;
+let bul = true;
+let bul1 = false;
+console.log(str);
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(num);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(bul);
+console.log(bul1);
+
+let firstName = 'Andriy'
+let middleName = 'Mukolayovuch'
+let lastName = 'Kiselov'
+let person = firstName + middleName + lastName
+console.log(person);
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
